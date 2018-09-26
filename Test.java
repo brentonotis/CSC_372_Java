a9e45d5b44f725e85f43c83b6f699aefdfd01774
Brenton Otis <brenton.otis@gmail.com>
2018-09-26 12:40:32 -0500

    Changed from flow to border layout

--- Test.java
+++ Test.java
@@ -1,17 +1,16 @@
-package test;
-
-import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Test {
 	
 	public static void main(String[] args) {
 		
 		JFrame frame = new JFrame("Test");
+		JPanel panel = new JPanel();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton button = new JButton("Blue");
@@ -35,12 +34,23 @@ public class Test {
 			}
 		});
 		
-		frame.getContentPane().setLayout(new FlowLayout());
-		frame.getContentPane().add(button);
-		frame.getContentPane().add(button2);
-		frame.getContentPane().add(button3);
+		JButton button4 = new JButton("Red");
+		button4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.getContentPane().setBackground(Color.RED);
+			}
+		});
 		
-		frame.setSize(300,  300);
+		frame.getContentPane().setLayout(new BorderLayout());
+		
+		panel.add(button2);
+		panel.add(button3);
+		
+		frame.getContentPane().add(button, BorderLayout.EAST);
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		frame.getContentPane().add(button4, BorderLayout.WEST);
+		
+		frame.setSize(300, 300);
 		frame.setVisible(true);
 	}
 }

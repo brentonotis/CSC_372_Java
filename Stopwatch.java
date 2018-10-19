227c53d8fe5a2689299224a2cf40378ba8731e92
Brenton Otis <brenton.otis@gmail.com>
2018-10-18 19:50:00 -0500

    Stopwatch allowing user to measure algorithims performance

---
+++ Stopwatch.java
@@ -0,0 +1,76 @@
+package selection_sort;
+
+import java.util.Scanner;
+/**
+ * Stop watch that accumulates time when running
+ * Measures elapsed time
+ * Can be used to measure run-time of methods/program/etc.
+ */
+
+public class Stopwatch {
+	
+	private long elapsedTime;
+	private long startTime;
+	private boolean isRunning;
+	
+	// Constructor - build stop watch that is in a stopped state/has no elapsed time
+	public Stopwatch() {
+		reset();
+	}
+	
+	// Helper method - starts stop watch, time starts accumulating
+	public void start() {
+		if (isRunning) {
+			return;
+		}
+		isRunning = true;
+		startTime = System.currentTimeMillis();
+	}
+	
+	// Helper method - stops stop watch, time stops accumulating and is added to elapsed time
+	public void stop() {
+		if (!isRunning) {
+			return;
+		}
+		isRunning = false;
+		long endTime = System.currentTimeMillis();
+		elapsedTime = elapsedTime + endTime - startTime;
+	}
+	
+	// Helper method - returns elapsed time
+	public long getElapsedTime() {
+		if (isRunning) {
+			long endTime = System.currentTimeMillis();
+			return elapsedTime + endTime - startTime;
+		}
+		else {
+			return elapsedTime;
+		}
+	}
+	
+	// Stops the stop watch and resets elapsed time to 0.0
+	public void reset() {
+		elapsedTime = 0;
+		isRunning = false;
+	}
+	
+	// Driver
+	@SuppressWarnings("resource")
+	public static void main (String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter array size: ");
+		int n = in.nextInt();
+		
+		// Construct random array
+		int[] a = SelectionSorter.randomIntArray(n,  100);
+		
+		// Use stop watch to time selection sort
+		Stopwatch timer = new Stopwatch();
+		
+		timer.start();
+		SelectionSorter.sort(a);
+		timer.stop();
+		
+		System.out.println("Elapsed time: " + timer.getElapsedTime() + " milliseconds");
+	}
+}

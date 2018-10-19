daffb31298a5462d53c741016b764cdf4f9fe49c
Brenton Otis <brenton.otis@gmail.com>
2018-10-18 23:28:49 -0500

    Insertion sort example (in selection_sort pkg)

---
+++ InsertionSort.java
@@ -0,0 +1,21 @@
+package selection_sort;
+/**
+ * Sorts array using insertion sort
+ * @param - array to sort
+ */
+public class InsertionSort {
+
+		public static void sort(int[] a) {
+			for (int i = 1; i < a.length; ++i) {
+				int next = a[i];
+				// Move all larger elements up
+				int j = i;
+				while (j > 0 && a[j -1] > next) {
+					a[j] = a[j -1];
+					j--;
+				}
+				// Insert the element
+				a[j] = next;
+			}
+		}
+}

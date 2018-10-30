56f7249a5c265c10127e7405ad950652acbe86cc
Brenton Otis <36424273+brentonotis@users.noreply.github.com>
2018-10-29 23:28:21 -0500

    Update SelectionSort.java
    Per instructor feedback added method to sort in alphabetical order (in addition to reversed alphabetical order)

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,7 +1,8 @@
 package selection_sorter;
 
 /**
- * Class using selection sort to return array list of names in reversed alphabetical order
+ * Class using selection sort to return array list of names in alphabetical order
+ * Also sorts in reversed alphabetical order
  * Gets array list of names from user input
  * @author Brenton Otis
  */
@@ -18,8 +19,12 @@ public class SelectionSort {
 	public static void main (String[] args) {
 		getUserNames();
 		System.out.println("Unsorted array of names: " + userNames);
+		
 		sort(userNames);
-		System.out.println("Sorted array of names: " + userNames);
+		System.out.println("Sorted alphabetically: " + userNames);
+		
+		sortReverse(userNames);
+		System.out.println("Sorted alphabetically and in reverse: " + userNames);
 	}
 	
 	/**
@@ -39,7 +44,32 @@ public class SelectionSort {
 	}
 	
 	/**
-	 * Sorts array list via selection sort algorithm 
+	 * Sorts array list via selection sort algorithm in reverse alphabetical order
+	 * Implements compareTo() method from Comparable interface
+	 * @param userNames - array to sort
+	 */	
+	public static void sortReverse (ArrayList<String> userNames) {
+		
+		// Loops through array list, sets minIndex at each pass
+		for (int i = 0; i < userNames.size() - 1; i++) {
+			int minIndex = i;
+			// Loops through array list after evaluated position (i)
+			for (int j = i + 1; j < userNames.size(); ++j) {
+				// If second position > evaluated position, sets minIndex at next position
+				// Uses compareTo() method since String type implements Comparable interface
+				if (userNames.get(j).compareTo(userNames.get(minIndex)) > 0) {
+					minIndex = j;
+				}
+			}
+			// Swaps the two entries
+			String temp = userNames.get(i);
+			userNames.set(i, userNames.get(minIndex));
+			userNames.set(minIndex, temp);
+		}
+	}
+	
+	/**
+	 * Sorts array list via selection sort algorithm in alphabetical order
 	 * Implements compareTo() method from Comparable interface
 	 * @param userNames - array to sort
 	 */	
@@ -52,7 +82,7 @@ public class SelectionSort {
 			for (int j = i + 1; j < userNames.size(); ++j) {
 				// If second position > evaluated position, sets minIndex at next position
 				// Uses compareTo() method since String type implements Comparable interface
-				if (userNames.get(j).compareTo(userNames.get(minIndex)) > 0) {
+				if (userNames.get(j).compareTo(userNames.get(minIndex)) < 0) {
 					minIndex = j;
 				}
 			}
